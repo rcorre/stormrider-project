2c20bd4808f2a2d74c0ae74c78bd2af032d2de4f
Ryan RC <murphyslaw480@gmail.com>
2014-11-14 11:20:54 -0500

    apply texture and uvs

--- Assets/Scripts/TileMap.cs
+++ Assets/Scripts/TileMap.cs
@@ -21,6 +21,7 @@ public class TileMap : MonoBehaviour {
         Vector3[] vertices = new Vector3[4];
         int[] triangles = new int[2 * 3];
         Vector3[] normals = new Vector3[4];
+        Vector2[] uv = new Vector2[4];
 
         // generate mesh data
         vertices[0] = new Vector3(0, 0, 0);
@@ -41,11 +42,18 @@ public class TileMap : MonoBehaviour {
         normals[2] = Vector3.up;
         normals[3] = Vector3.up;
 
+        // generate mesh data
+        uv[0] = new Vector2(0, 1);
+        uv[1] = new Vector2(0, 0);
+        uv[2] = new Vector2(1, 1);
+        uv[3] = new Vector2(1, 0);
+
         // create and populate a new mesh
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.normals = normals;
+        mesh.uv = uv;
 
         // apply mesh to filter/renderer/collider
         ApplyMesh(mesh);
