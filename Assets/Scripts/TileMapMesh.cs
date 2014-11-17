f8540d9e38a9bd26bac4ddc0c8385e656cf88d4f
Ryan RC <murphyslaw480@gmail.com>
2014-11-17 14:20:26 -0500

    use meshData object to simplify code

--- Assets/Scripts/TileMapMesh.cs
+++ Assets/Scripts/TileMapMesh.cs
@@ -25,10 +25,7 @@ public class TileMapMesh : MonoBehaviour {
         // each tile contains two triangles
         int numTriangles = numTiles * 2;
 
-        var vertices = new List<Vector3>();
-        var normals = new List<Vector3>();
-        var uv = new List<Vector2>();
-        var triangles = new List<int>();
+	var meshData = new MeshData();
 
         for (int row = 0; row < map.numRows; row++) {
             for (int col = 0; col < map.numCols; col++) {
@@ -40,59 +37,52 @@ public class TileMapMesh : MonoBehaviour {
                 float left = col * tileSize;
                 float right = col * tileSize + tileSize;
                 // get the indices of the 4 vertices owned by this tile
-                int v0 = vertices.Count;
+                int v0 = meshData.vertices.Count;
                 int v1 = v0 + 1;
                 int v2 = v0 + 2;
                 int v3 = v0 + 3;
                 // assign world-space positions to the vertices
-                vertices.Add(new Vector3(left, height, top));
-                vertices.Add(new Vector3(right, height, top));
-                vertices.Add(new Vector3(left, height, bottom));
-                vertices.Add(new Vector3(right, height, bottom));
+                meshData.vertices.Add(new Vector3(left, height, top));
+                meshData.vertices.Add(new Vector3(right, height, top));
+                meshData.vertices.Add(new Vector3(left, height, bottom));
+                meshData.vertices.Add(new Vector3(right, height, bottom));
                 // all normals point up for flat surface
-                normals.Add(Vector3.up);
-                normals.Add(Vector3.up);
-                normals.Add(Vector3.up);
-                normals.Add(Vector3.up);
+                meshData.normals.Add(Vector3.up);
+                meshData.normals.Add(Vector3.up);
+                meshData.normals.Add(Vector3.up);
+                meshData.normals.Add(Vector3.up);
                 // TODO: compute uvs based on terrain type, match corresponding point on texture
                 float uvX = (float)col / numCols;
                 float uvY = (float)row / numCols;
-                uv.Add(new Vector2(uvX, uvY));
-                uv.Add(new Vector2(uvX, uvY));
-                uv.Add(new Vector2(uvX, uvY));
-                uv.Add(new Vector2(uvX, uvY));
+                meshData.uv.Add(new Vector2(uvX, uvY));
+                meshData.uv.Add(new Vector2(uvX, uvY));
+                meshData.uv.Add(new Vector2(uvX, uvY));
+                meshData.uv.Add(new Vector2(uvX, uvY));
                 // assign vertex indices to the two triangles owned by this tile
-                triangles.Add(v0);
-                triangles.Add(v3);
-                triangles.Add(v2);
-                triangles.Add(v0);
-                triangles.Add(v1);
-                triangles.Add(v3);
+                meshData.triangles.Add(v0);
+                meshData.triangles.Add(v3);
+                meshData.triangles.Add(v2);
+                meshData.triangles.Add(v0);
+                meshData.triangles.Add(v1);
+                meshData.triangles.Add(v3);
                 if (col < numCols - 1) { // create side mesh to next tile
                     var tileToRight = map.tileAt(row, col + 1);
-                    AddSideX(tile, tileToRight, vertices, normals, uv, triangles, uvX, uvY);
+                    AddSideX(tile, tileToRight, meshData, uvX, uvY);
                 }
 
                 if (row < numRows - 1) { // create side mesh to next tile
                     var tileAbove = map.tileAt(row + 1, col);
-                    AddSideZ(tile, tileAbove, vertices, normals, uv, triangles, uvX, uvY);
+                    AddSideZ(tile, tileAbove, meshData, uvX, uvY);
                 }
             }
         }
 
-        // create and populate a new mesh
-        Mesh mesh = new Mesh();
-        mesh.vertices = vertices.ToArray();
-        mesh.triangles = triangles.ToArray();
-        mesh.normals = normals.ToArray();
-        mesh.uv = uv.ToArray();
-
         // apply mesh to filter/renderer/collider
-        ApplyMesh(mesh);
+        ApplyMesh(meshData);
         BuildTexture();
     }
 
-    void AddSideX(Tile tile, Tile next, List<Vector3> vertices, List<Vector3> normals, List<Vector2> uv, List<int> triangles, float uvX, float uvY) {
+    void AddSideX(Tile tile, Tile next, MeshData meshData, float uvX, float uvY) {
         float height = tile.elevation * heightScale;
         float left = tile.col * tileSize;
         float right = tile.col * tileSize + tileSize;
@@ -103,36 +93,36 @@ public class TileMapMesh : MonoBehaviour {
         if (diff != 0) {
             var norm = diff > 0 ? Vector3.left : Vector3.right;
             var nextHeight = next.elevation * heightScale;
-            int v0 = vertices.Count;
+            int v0 = meshData.vertices.Count;
             int v1 = v0 + 1;
             int v2 = v0 + 2;
             int v3 = v0 + 3;
 
-            vertices.Add(new Vector3(right, height, top));
-            vertices.Add(new Vector3(right, nextHeight, top));
-            vertices.Add(new Vector3(right, height, bottom));
-            vertices.Add(new Vector3(right, nextHeight, bottom));
+            meshData.vertices.Add(new Vector3(right, height, top));
+            meshData.vertices.Add(new Vector3(right, nextHeight, top));
+            meshData.vertices.Add(new Vector3(right, height, bottom));
+            meshData.vertices.Add(new Vector3(right, nextHeight, bottom));
 
-            normals.Add(norm);
-            normals.Add(norm);
-            normals.Add(norm);
-            normals.Add(norm);
+            meshData.normals.Add(norm);
+            meshData.normals.Add(norm);
+            meshData.normals.Add(norm);
+            meshData.normals.Add(norm);
 
-            triangles.Add(v0);
-            triangles.Add(v3);
-            triangles.Add(v2);
-            triangles.Add(v0);
-            triangles.Add(v1);
-            triangles.Add(v3);
+            meshData.triangles.Add(v0);
+            meshData.triangles.Add(v3);
+            meshData.triangles.Add(v2);
+            meshData.triangles.Add(v0);
+            meshData.triangles.Add(v1);
+            meshData.triangles.Add(v3);
 
-            uv.Add(new Vector2(uvX, uvY));
-            uv.Add(new Vector2(uvX, uvY));
-            uv.Add(new Vector2(uvX, uvY));
-            uv.Add(new Vector2(uvX, uvY));
+            meshData.uv.Add(new Vector2(uvX, uvY));
+            meshData.uv.Add(new Vector2(uvX, uvY));
+            meshData.uv.Add(new Vector2(uvX, uvY));
+            meshData.uv.Add(new Vector2(uvX, uvY));
         }
     }
 
-    void AddSideZ(Tile tile, Tile next, List<Vector3> vertices, List<Vector3> normals, List<Vector2> uv, List<int> triangles, float uvX, float uvY) {
+    void AddSideZ(Tile tile, Tile next, MeshData meshData, float uvX, float uvY) {
         float height = tile.elevation * heightScale;
         float left   = tile.col * tileSize;
         float right  = tile.col * tileSize + tileSize;
@@ -143,32 +133,32 @@ public class TileMapMesh : MonoBehaviour {
         if (diff != 0) {
             var norm = diff > 0 ? Vector3.back : Vector3.forward;
             var nextHeight = next.elevation * heightScale;
-            int v0 = vertices.Count;
+            int v0 = meshData.vertices.Count;
             int v1 = v0 + 1;
             int v2 = v0 + 2;
             int v3 = v0 + 3;
 
-            vertices.Add(new Vector3(left, nextHeight, top));
-            vertices.Add(new Vector3(right, nextHeight, top));
-            vertices.Add(new Vector3(left, height, top));
-            vertices.Add(new Vector3(right, height, top));
+            meshData.vertices.Add(new Vector3(left, nextHeight, top));
+            meshData.vertices.Add(new Vector3(right, nextHeight, top));
+            meshData.vertices.Add(new Vector3(left, height, top));
+            meshData.vertices.Add(new Vector3(right, height, top));
 
-            normals.Add(norm);
-            normals.Add(norm);
-            normals.Add(norm);
-            normals.Add(norm);
+            meshData.normals.Add(norm);
+            meshData.normals.Add(norm);
+            meshData.normals.Add(norm);
+            meshData.normals.Add(norm);
 
-            triangles.Add(v0);
-            triangles.Add(v3);
-            triangles.Add(v2);
-            triangles.Add(v0);
-            triangles.Add(v1);
-            triangles.Add(v3);
+            meshData.triangles.Add(v0);
+            meshData.triangles.Add(v3);
+            meshData.triangles.Add(v2);
+            meshData.triangles.Add(v0);
+            meshData.triangles.Add(v1);
+            meshData.triangles.Add(v3);
 
-            uv.Add(new Vector2(uvX, uvY));
-            uv.Add(new Vector2(uvX, uvY));
-            uv.Add(new Vector2(uvX, uvY));
-            uv.Add(new Vector2(uvX, uvY));
+            meshData.uv.Add(new Vector2(uvX, uvY));
+            meshData.uv.Add(new Vector2(uvX, uvY));
+            meshData.uv.Add(new Vector2(uvX, uvY));
+            meshData.uv.Add(new Vector2(uvX, uvY));
         }
     }
 
@@ -190,12 +180,36 @@ public class TileMapMesh : MonoBehaviour {
         renderer.sharedMaterials[0].mainTexture = texture;
     }
 
-    void ApplyMesh(Mesh mesh) {
+    void ApplyMesh(MeshData meshData) {
+        // create and populate a new mesh
+        Mesh mesh = new Mesh();
+        mesh.vertices  = meshData.vertices.ToArray();
+        mesh.triangles = meshData.triangles.ToArray();
+        mesh.normals   = meshData.normals.ToArray();
+        mesh.uv        = meshData.uv.ToArray();
+        mesh.Optimize(); // this could increase performance but also incurs higher generation time
+
         var filter = GetComponent<MeshFilter>();
         var collider = GetComponent<MeshCollider>();
 
-        mesh.Optimize(); // this could increase performance but also incurs higher generation time
         filter.mesh = mesh;
         collider.sharedMesh = mesh;
+    }
+
+    /// <summary>
+    /// convenience container for storing related mesh data during generation
+    /// </summary>
+    private class MeshData {
+        public MeshData() {
+            vertices = new List<Vector3>();
+            normals = new List<Vector3>();
+            uv = new List<Vector2>();
+            triangles = new List<int>();
+
+        }
+        public List<Vector3> vertices;
+        public List<Vector3> normals;
+        public List<Vector2> uv;
+        public List<int> triangles;
     }
 }
