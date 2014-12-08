bbd41fefd22a201fb7da671d05819fe98ff13061
Ryan RC <murphyslaw480@gmail.com>
2014-12-08 14:27:06 -0500

    show path for tile if in move range

--- Assets/Scripts/Battle/States/UnitSelected.cs
+++ Assets/Scripts/Battle/States/UnitSelected.cs
@@ -1,10 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class UnitSelected : State<Battle> {
     private Battler _battler;
     private PathFinder _pathFinder;
     private TileOverlay _overlay;
     private Tile[] _moveRange;
+    private Tile _tileUnderMouse;
+    private Stack<Tile> _movePath;
 
     public UnitSelected(Battler battler) {
         _battler = battler;
@@ -21,5 +24,16 @@ public class UnitSelected : State<Battle> {
     public override void Update(Battle battle) {
         base.Update(battle);
         var tile = battle.map.mouse.tileUnderMouse;
+        if (tile != null && _tileUnderMouse != tile) { // new tile under mouse
+            if (_pathFinder.CostToTile(tile) <= _battler.MoveRange) {
+                _movePath = _pathFinder.PathToTile(tile);
+            }
+            else {
+                _movePath = null;
+            }
+            if (_movePath != null) {
+                _overlay.DrawPath(_movePath);
+            }
+        }
     }
 }
