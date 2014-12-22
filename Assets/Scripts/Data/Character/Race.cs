44e76ad7b6df851eb2c8752482ec1e6b387dcc5c
Ryan RC <murphyslaw480@gmail.com>
2014-12-21 21:29:22 -0500

    add character race

---
+++ Assets/Scripts/Data/Character/Race.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using FullSerializer;
+
+public class Race {
+    public readonly string key;
+    public readonly string name;
+    public readonly int speed;
+    public readonly int initiative;
+    public readonly int health;
+    public readonly int stamina;
+    public readonly int weightCapacity;
+    public readonly int fortitude;
+    public readonly int willpower;
+    public readonly AttributeSet attributes;
+    public readonly ElementSet baseArmor;
+}
