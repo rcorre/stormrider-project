07c62c99101b00faf9c6c8113e9cdf806e3d351e
Ryan RC <murphyslaw480@gmail.com>
2015-01-10 18:44:17 -0500

    show loaded character stats

---
+++ Assets/Scripts/UI/CharacterSheet/CharacterSheetUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSheetUI : MonoBehaviour {
+    public string characterName;
+
+    private Character _character;
+    public Character character {
+        get {
+            if (_character == null && characterName != null) {
+                _character = DataManager.Fetch<Character>(characterName);
+            }
+            return _character;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/CharacterSheet/CharacterStatText.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum CharacterStat {
+    BaseAttribute,
+    EffectiveAttribute,
+}
+
+[RequireComponent(typeof(Text))]
+public class CharacterStatText : MonoBehaviour {
+    public CharacterStat stat;
+    public CharacterAttribute attribute;
+    private Character _character;
+    private Text _text;
+
+    // Use this for initialization
+    void Start() {
+        _character = FindObjectOfType<CharacterSheetUI>().character;
+        _text = GetComponent<Text>();
+        SetText();
+    }
+
+    public void SetText() {
+        int val = _character.baseAttributes[attribute];
+        _text.text = val.ToString();
+    }
+}
