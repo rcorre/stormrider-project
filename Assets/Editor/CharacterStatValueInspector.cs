2e978d3a350598565b447d37412588e7ea0a7041
Ryan RC <murphyslaw480@gmail.com>
2015-01-11 15:21:52 -0500

    custom inspector for stat-value gui

---
+++ Assets/Editor/CharacterStatValueInspector.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+[ExecuteInEditMode]
+[CustomEditor(typeof(CharacterStatText))]
+public class CharacterStatTextEditor : Editor {
+    public override void OnInspectorGUI() {
+        var statText = (CharacterStatText)target;
+
+        EditorGUILayout.BeginVertical();
+
+        statText.stat = selectEnum<CharacterStat>(statText.stat);
+        switch (statText.stat) {
+            case CharacterStat.BaseAttribute:
+            case CharacterStat.EffectiveAttribute:
+                statText.attribute = selectEnum<CharacterAttribute>(statText.attribute);
+                break;
+        }
+
+        EditorGUILayout.EndVertical();
+    }
+
+    private T selectEnum<T>(Enum current) {
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.PrefixLabel(typeof(T).ToString());
+	var selection = EditorGUILayout.EnumPopup(current);
+
+        EditorGUILayout.EndHorizontal();
+
+        return (T)Convert.ChangeType(selection, typeof(T));
+    }
+}
