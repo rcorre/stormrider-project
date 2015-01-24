b41041a7f0b9a2adea9e2c3563b4ef663d9cbbc7
Ryan RC <murphyslaw480@gmail.com>
2015-01-24 18:05:40 -0500

    ui response to mouse enter

---
+++ Assets/Scripts/UI/HasToolTip.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(RectTransform))]
+public class HasToolTip : MonoBehaviour {
+    public string toolTipText;
+    private RectTransform _area;
+    private ToolTipManager _manager;
+
+    void Awake() {
+        _area = GetComponent<RectTransform>();
+        _manager = FindObjectOfType<ToolTipManager>();
+    }
+
+    void Start() {
+	// hook up onMouseEnter handler to activate tooltip
+        var trigger = gameObject.AddComponent<EventTrigger>();
+	var entry   = new EventTrigger.Entry();
+
+	entry.eventID  = EventTriggerType.PointerEnter;
+	entry.callback = new EventTrigger.TriggerEvent();
+
+	UnityAction<BaseEventData> callback = 
+	    new UnityAction<BaseEventData>(MouseEnterMethod);
+
+	entry.callback.AddListener(callback);
+
+	trigger.delegates = new List<EventTrigger.Entry>();
+	trigger.delegates.Add(entry);
+    }
+
+    public void MouseEnterMethod(BaseEventData baseEvent) {
+        Util.Assert(_manager != null, "manager null");
+        _manager.SetText(toolTipText);
+    }
+}
---
+++ Assets/Scripts/UI/ToolTipManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ToolTipManager : MonoBehaviour {
+    public Text textObject;
+
+    void Start() {
+        gameObject.SetActive(false);
+    }
+
+    void Update() {
+        textObject.transform.position = Input.mousePosition;
+    }
+
+    public void SetText(string text) {
+        gameObject.SetActive(true);
+        textObject.text = text;
+    }
+
+    public void ClearText() {
+        gameObject.SetActive(false);
+    }
+}
