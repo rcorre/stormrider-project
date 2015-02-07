887dd78d7226e4e4cd54ad93a3a47bcac8c007a8
Ryan RC <murphyslaw480@gmail.com>
2015-02-06 20:50:29 -0500

    work on tooltip implementation

--- Assets/Scripts/UI/HasToolTip.cs
+++ Assets/Scripts/UI/HasToolTip.cs
@@ -6,43 +6,20 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(RectTransform))]
-public class HasToolTip : MonoBehaviour {
+public class HasToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
     public string toolTipText;
-    private RectTransform _area;
     private ToolTipManager _manager;
 
     void Awake() {
-        _area = GetComponent<RectTransform>();
 	// grab manager in awake, it may be inactive during start
         _manager = FindObjectOfType<ToolTipManager>();
     }
 
-    void Start() {
-	// set up mouse handlers
-	var handlers = new List<EventTrigger.Entry>();
-
-	// set tooltip text on mouse enter
-	handlers.Add(CreateHandler(EventTriggerType.PointerEnter,
-	    (ev) => _manager.SetText(toolTipText)));
-
-	// clear tooltip on mouse leave
-	handlers.Add(CreateHandler(EventTriggerType.PointerExit,
-	    (ev) => _manager.ClearText()));
-
-	// attach an event trigger with the given delegates
-        var trigger = gameObject.AddComponent<EventTrigger>();
-	trigger.delegates = handlers;
+    public void OnPointerEnter(PointerEventData eventData) {
+        _manager.SetText(toolTipText);
     }
 
-    // return an event trigger entry that calls action upon receiving an event of the given type 
-    EventTrigger.Entry CreateHandler(EventTriggerType type, Action<BaseEventData> action) {
-        var entry = new EventTrigger.Entry();
-        entry.eventID = type;
-
-        entry.callback = new EventTrigger.TriggerEvent();
-        var listener = new UnityAction<BaseEventData>(action);
-
-        entry.callback.AddListener(listener);
-        return entry;
+    public void OnPointerExit(PointerEventData eventData) {
+        _manager.ClearText();
     }
 }
--- Assets/Scripts/UI/ToolTipManager.cs
+++ Assets/Scripts/UI/ToolTipManager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class ToolTipManager : MonoBehaviour {
     public Text textObject;
+    private bool _textSet, _textCleared;
 
     void Start() {
         gameObject.SetActive(false);
@@ -11,14 +12,23 @@ public class ToolTipManager : MonoBehaviour {
 
     void Update() {
         textObject.transform.position = Input.mousePosition;
+        if (_textSet) {
+            gameObject.SetActive(true);
+        }
+        else if (_textCleared) {
+            gameObject.SetActive(false);
+        }
+        _textSet = false;
+	_textCleared = false;
     }
 
     public void SetText(string text) {
+        _textSet = true;
         gameObject.SetActive(true);
         textObject.text = text;
     }
 
     public void ClearText() {
-        gameObject.SetActive(false);
+        _textCleared = true;
     }
 }
